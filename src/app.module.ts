16dd28a686691d396b0baf378396bc43a96990a6
Naotsugu Someya <chome.au.lait.github@gmail.com>
2022-01-07 09:47:24 +0900

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [AppModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
